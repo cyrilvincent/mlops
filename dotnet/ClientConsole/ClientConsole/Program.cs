40250942c0bd91d189a7714a66fc2f56b83e703e
Cyril Vincent <contact@cyrilvincent.com>
2023-11-03 15:39:07 +0100

    dotnet

---
+++ dotnet/ClientConsole/ClientConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Client API in C#");
+string url = "http://127.0.0.1:5000/house";
+while (true)
+{
+    Console.Write("Saisir une surface: ");
+    string s = Console.ReadLine();
+    if (s == "") break;
+    var client = new HttpClient();
+    Console.WriteLine($"Appel de {url}/{s}");
+    var result = await client.GetAsync($"{url}/{s}");
+    var json = await result.Content.ReadAsStringAsync();
+    var loyer = float.Parse(json);
+    Console.WriteLine($"Loyer estimé: {(int)loyer} Euro");
+}
+
+
