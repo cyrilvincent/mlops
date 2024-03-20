3f2bc2e6481283a1073d182cc9a1bc7673114133
Cyril Vincent <contact@cyrilvincent.com>
2024-03-20 15:46:29 +0100

    Sopra

--- tp_cancer.py
+++ tp_cancer.py
@@ -1,6 +1,8 @@
 import pandas
 import pandas as pd
 import sklearn.ensemble as tree
+import sklearn.svm as svm
+import sklearn.neighbors as nn
 import pickle
 import matplotlib.pyplot as plt
 
@@ -9,29 +11,32 @@ y = dataframe.diagnosis
 x = dataframe.drop(["diagnosis"], axis=1)
 
 model = tree.RandomForestClassifier()
+#model = svm.SVC()
+#model = nn.KNeighborsClassifier()
+
 model.fit(x, y)
 score = model.score(x, y)
 print(score)
 
-path = f"data/cancer/rf-{int(score*100)}.pickle" # Save
+path = f"data/cancer/svm-{int(score*100)}.pickle" # Save
 with open(path, "wb") as f:
     pickle.dump(model, f)
 
 y_pred = model.predict(x)
 print(y_pred)
 
-print(model.feature_importances_)
-plt.bar(x.columns, model.feature_importances_)
-plt.xticks(rotation=45)
-plt.show()
-
-from sklearn.tree import export_graphviz
-export_graphviz(model.estimators_[0],
-                 out_file='data/cancer/tree.dot',
-                 feature_names = x.columns,
-                 class_names = ["0", "1"],
-                 rounded = True, proportion = False,
-                 precision = 2, filled = True)
+# print(model.feature_importances_)
+# plt.bar(x.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
+#
+# from sklearn.tree import export_graphviz
+# export_graphviz(model.estimators_[0],
+#                  out_file='data/cancer/tree.dot',
+#                  feature_names = x.columns,
+#                  class_names = ["0", "1"],
+#                  rounded = True, proportion = False,
+#                  precision = 2, filled = True)
 
 # Créer le modèle RF
 # Fit
