298dbfb7585bec5214e7a3fb0ef7be29dfe47bec
Cyril Vincent <contact@cyrilvincent.com>
2024-03-21 08:59:35 +0100

    Sopra

---
+++ tp_cancer_tf.py
@@ -0,0 +1,27 @@
+import pickle
+
+import sklearn.preprocessing
+import tensorflow as tf
+import pandas
+
+
+tf.random.set_seed(1)
+
+dataframe = pandas.read_csv("data/cancer/data.csv", index_col="id")
+y = dataframe.diagnosis
+x = dataframe.drop("diagnosis", 1)
+
+model = tf.keras.Sequential([
+    # todo
+    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+  ])
+
+model.compile(loss="binary_crossentropy", metrics=['accuracy'])
+# for 2 categories bce ~= cce
+model.summary()
+
+# Fit
+# Evaluate
+# Save
+
+
