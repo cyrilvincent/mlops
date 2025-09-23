fde409e06ae61e495a5525aeeb9e1b8f0e01efd0
Cyril Vincent <contact@cyrilvincent.com>
2025-09-23 14:19:42 +0200

    Sparks

--- Dockerfile.tf
+++ Dockerfile.tf
@@ -8,7 +8,7 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /app
 
 RUN --mount=type=cache,target=/root/.cache/pip \
-    --mount=type=bind,source=requirements-tf.txt,target=requirements.txt \
+    --mount=type=bind,source=requirements.txt,target=requirements.txt \
     python -m pip install -r requirements.txt
 RUN mkdir -p temp
 
